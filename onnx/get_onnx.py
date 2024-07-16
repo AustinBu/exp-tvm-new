a9e4860739c58f68e1caa323b910e9dc66497296
Bowen Yao <bowen@Bowens-MacBook-Pro.local>
2024-07-16 14:52:37 -0700

    minor change

--- onnx/get_onnx.py
+++
@@ -1,6 +0,0 @@
-import urllib.request
-
-# Download a simple ONNX model
-url = 'https://github.com/onnx/models/blob/main/validated/vision/classification/mnist/model/mnist-1.onnx'
-onnx_model_path = 'mnist-1.onnx'
-urllib.request.urlretrieve(url, onnx_model_path)
